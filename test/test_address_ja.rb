4cab180f5ddd63252db4720511877ca937c8fb5c
KobayashiTaishi <kbystis@gmail.com>
2013-12-14 00:48:57 +0900

    jfalsd

--- test/test_address_ja.rb
+++ test/test_address_ja.rb
@@ -2,8 +2,6 @@ require 'helper'
 
 class TestAddressJA < Test::Unit::TestCase
   def test_prefectures
-		puts  Faker::AddressJA::PREFECTURES
-		puts  Faker::AddressJA.prefectures
     assert Faker::AddressJA::PREFECTURES.include?(Faker::AddressJA.prefectures)
   end
 
